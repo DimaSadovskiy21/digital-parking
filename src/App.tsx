ff536263f22ef76b0a19c2f5f4dc16e058c1d5ba
DimaSadovskiy <dmitriy.it@piar.ee>
2025-01-31 14:53:16 +0300

    refactor: rewrote the name of the carNumbers folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 // project imports
 import Layout from "./components/Layout";
-import CarNumbers from "./features/CarNumbers";
+import CarNumbers from "./features/carNumbers";
 import Parking from "./features/parking";
 
 const App = () => {
